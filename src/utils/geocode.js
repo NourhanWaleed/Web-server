45132dff2479ed79852dbb29527b3f8fb8a69d19
Nourhan Waleed <nouraaboelsoaoud@yahoo.com>
2022-08-05 18:44:48 +0200

    initial commit

---
+++ src/utils/geocode.js
@@ -0,0 +1,21 @@
+const request = require('request')
+
+const geocode = (address, callback) => {
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1Ijoibm91cmhhbndhbGVlZCIsImEiOiJjbDZhb2lhcWQ0Z296M29wZGZ4dzI3c2JnIn0.obQvBWRTD8wH1TOY14erQg'
+
+    request({url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to location services!', undefined)
+        } else if (body.features.length === 0) {
+            callback('Unable to find location. Try another search.', undefined)
+        } else {
+            callback(undefined, {
+                latitude: body.features[0].geometry.coordinates[1],
+                longitude: body.features[0].geometry.coordinates[0],
+                location: body.features[0].place_name
+            })
+        }
+    })
+}
+
+module.exports = geocode
